467eec81984ae0fb22a1119e7bdf13bfe666c028
devdinei <devdinei@gmail.com>
2024-11-12 20:52:53 -0300

    Create npm firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDcpggR7jf2BEPNLqRj1Iz368F0dDtD1-4",
+  authDomain: "planilha-8938f.firebaseapp.com",
+  projectId: "planilha-8938f",
+  storageBucket: "planilha-8938f.firebasestorage.app",
+  messagingSenderId: "211015132743",
+  appId: "1:211015132743:web:45f443dc9e65b72fe37362"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
